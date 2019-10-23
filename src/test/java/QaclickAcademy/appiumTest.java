5c5f7a5e37abaea376e8f7dbbfe0c7001f1c353e
ANJALY <anair8388@gmail.com>
2019-10-23 12:23:17 +0100

    Commited by anjaly

--- src/test/java/QaclickAcademy/appiumTest.java
+++ src/test/java/QaclickAcademy/appiumTest.java
@@ -6,5 +6,7 @@ public class appiumTest {
 	@Test
 	public void appiumtest1() {
 		System.out.println("open browser");
+		System.out.println("driver initialization");
+		System.out.println("driver initialization");
 }
 }
